4677400f6d8391bf21207e918cc62795c93acd16
morishita <morishixxx@gmail.com>
2021-03-12 06:41:49 +0000

    update

--- resources/views/tasklists/index.blade.php
+++ resources/views/tasklists/index.blade.php
@@ -26,8 +26,8 @@
             </tbody>
         </table>
     @endif
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {{-- タスク作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
  
 
 
